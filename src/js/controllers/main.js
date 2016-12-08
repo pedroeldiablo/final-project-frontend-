3ab854a373ce6c2dd3d27838b13452d7f29f0d3f
Peter Williams <pedroeldiablo@yahoo.com>
2016-12-08 17:42:48 +0000

    initial commit

---
+++ src/js/controllers/main.js
@@ -0,0 +1,31 @@
+angular.module('finalProject')
+  .controller( 'MainController', MainController);
+
+MainController.$inject = ['$auth', '$state', '$rootScope'];
+function MainController($auth, $state, $rootScope) {
+  const main = this;
+  main.isLoggedIn = $auth.isAuthenticated;
+  main.message = null;
+
+  function logout() {
+    $auth.logout()
+      .then(() => {
+        $state.go('usersIndex');
+      });
+  }
+
+  const protectedStates = ['usersEdit', 'usersShow'];
+
+  function secureState(e, toState) {
+    main.message = null;
+    if(!$auth.isAuthenticated() &&  protectedStates.includes(toState.name)) {
+      e.preventDefault();
+      $state.go('login');
+      main.message= 'You must be logged in to go there!';
+    }
+  }
+
+  $rootScope.$on('$stateChangeStart', secureState);
+
+  main.logout = logout;
+}
